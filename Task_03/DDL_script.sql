23f7981f041315f7da85e7ef2dbf621c5f3da03f
Nastassia Zharnasek <Nastassia_Zharnasek@epam.com>
2018-05-11 14:38:39 +0300

    Added task_04

---
+++ Task_03/DDL_script.sql
@@ -0,0 +1,16 @@
+CREATE TABLE file_names(
+    file_name VARCHAR2 (250)
+);
+
+
+
+CREATE TABLE payment
+(
+    paymentid NUMBER(15),
+    customerid NUMBER(15),
+    productid NUMBER(15),
+    transactiondate VARCHAR2(255),
+    creditcard VARCHAR2(255),
+    creditcardnumber VARCHAR2(255)
+);
+
